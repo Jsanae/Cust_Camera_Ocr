55648fd1fb798af8b1a8a9d710bb83d8f65ad404
pasella <olivier.pasella@gmail.com>
2015-08-10 20:20:16 +0200

    pousser le rep complet  de dev vers github

---
+++ ARCHIVES_CODES/temp/CameraPreview.java
@@ -0,0 +1,23 @@
+//versus ggle ==>
+// MODIF TEST de git from asus + st3 camera_ocr vers github/camera_ocr
+// une modif depuis st3 et apres le chmod -R 777 
+// une autre modif depuis sr3 et wp luna
+package com.datumdroid.android.ocr.simple;
+
+import java.io.IOException;
+import java.util.List;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.ViewGroup;
+
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+	
+
+}
